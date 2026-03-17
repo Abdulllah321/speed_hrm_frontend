2576396b2e77aef01a0de578d7a827d19bf08a32
Abdulllah321 <abdullahsufyan2007@gmail.com>
2026-03-17 15:57:42 +0500

    .

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,7 @@ const nextConfig: NextConfig = {
     },
   },
   env: {
+    API_URL: process.env.API_URL,
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
     NEXT_SERVER_ACTIONS_ENCRYPTION_KEY: process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY,
   },
